20bba37adfeb4c3068a0e5b5b9b3e97992b45a05
François David <francois.david@ibm.com>
2018-05-24 01:47:34 +0200

    [RLE] Error w/ return format

--- src/main/scala/Algos/CompressAlgo.scala
+++ src/main/scala/Algos/CompressAlgo.scala
@@ -3,9 +3,12 @@ package Algos
 class CompressAlgo(var input: String) {
 
   def RLE() = (1 until this.input.size)
-    .foldLeft(new StringBuilder) {
-      case (sb, idx) => {
-        sb.append(idx)
-      }
+    .foldLeft((1, new StringBuilder(this.input(0).toString))) {
+      (acc, idx) =>
+          if (input(idx - 1) == this.input(idx)) (acc._1 + 1, acc._2)
+          else  {
+            acc._2.append(acc._1, this.input(idx))
+            (1, acc._2)
+        }
     }
 }
