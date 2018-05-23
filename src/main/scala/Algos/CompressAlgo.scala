359b8a4e813ab6d1fd6f4e182317a6b0a90eb79b
François David <francois.david@ibm.com>
2018-05-24 01:23:03 +0200

    [RLE] Now correctly iterate over String + building a new StringBuilder

--- src/main/scala/Algos/CompressAlgo.scala
+++ src/main/scala/Algos/CompressAlgo.scala
@@ -1,4 +1,11 @@
+package Algos
+
 class CompressAlgo(var input: String) {
 
-  def RLE() = println((1 until this.input.size))
+  def RLE() = (1 until this.input.size)
+    .foldLeft(new StringBuilder) {
+      case (sb, idx) => {
+        sb.append(idx)
+      }
+    }
 }
