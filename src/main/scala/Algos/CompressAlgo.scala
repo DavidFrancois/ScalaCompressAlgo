710147e7d4037dbf5e440782b2c578120799a5a3
François David <francois.david@ibm.com>
2018-05-24 15:51:14 +0200

    [RLE] Running ok except for last char sequence

--- src/main/scala/Algos/CompressAlgo.scala
+++ src/main/scala/Algos/CompressAlgo.scala
@@ -2,13 +2,17 @@ package Algos
 
 class CompressAlgo(var input: String) {
 
-  def RLE() = (1 until this.input.size)
-    .foldLeft((1, new StringBuilder(this.input(0).toString))) {
-      (acc, idx) =>
-          if (input(idx - 1) == this.input(idx)) (acc._1 + 1, acc._2)
-          else  {
-            acc._2.append(acc._1, this.input(idx))
-            (1, acc._2)
+  def RLE() = (1 until input.size)
+    .foldLeft((1, new StringBuilder)) {
+      case ((count, sb), idx) =>
+        if (idx == input.size || input(idx - 1) != input(idx)) {
+          sb.append(count)
+          sb.append(input(idx - 1))
+          (1, sb)
+        } else {
+          (count + 1, sb)
         }
-    }
+    } match {
+    case (count, sb) => sb.toString()
+  }
 }
