e46cca0a2ab0bbf3e0764d5defd37330f6ffc92a
François David <francois.david@ibm.com>
2018-05-24 19:29:20 +0200

    [TEST] First simple case for RLE properly tested

--- src/test/scala/RLETest.scala
+++ src/test/scala/RLETest.scala
@@ -1,3 +1,10 @@
-class RLETest {
+import Algos.CompressAlgo
+import org.scalatest.FunSuite
 
+class RLETest extends FunSuite {
+  var compAlg = new CompressAlgo()
+
+  test("Simple case") {
+    assert(compAlg.RLE("TTTTOOOOTTTTOOOO") == "4T4O4T4O")
+  }
 }
